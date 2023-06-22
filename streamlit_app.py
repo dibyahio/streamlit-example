c6756088f677707b7dd30676e1be2e112af59f1a
dibyahio <137046609+dibyahio@users.noreply.github.com>
2023-06-22 17:16:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,6 @@ forums](https://discuss.streamlit.io).
 Dibya
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-X = np.random.randint(100, size=(20))
-Y = np.random.randint(1000, size=(20))
-st.bar_chart(data=[X,Y], x=None, y=None, width=0, height=0, use_container_width=True)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -39,3 +36,6 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+X = np.random.randint(100, size=(20))
+Y = np.random.randint(1000, size=(20))
+st.bar_chart(data=[X,Y], x=None, y=None, width=0, height=0, use_container_width=True)
